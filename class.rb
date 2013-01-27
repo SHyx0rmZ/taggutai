6c7d4d91646d16f5b1137b451e63b7dc4b2d1724
Patrick Pokatilo <mail@shyxormz.net>
2013-01-28 00:33:01 +0100

    + Storage.import

--- class.rb
+++ class.rb
@@ -2,6 +2,8 @@
 # coding: utf-8
 
 require "yaml"
+require "digest/sha1"
+require "fileutils"
 
 config = YAML::load_file "config.yml"
 config[:paths] = {} unless config[:paths]
@@ -9,6 +11,9 @@ config[:paths] = {} unless config[:paths]
 STORAGE = config["paths"]["storage"] or "storage"
 TAGS = config["paths"]["tags"] or "tags"
 TRACKING = config["paths"]["tracking"] or "meta"
+IMPORT = config["paths"]["import"] or "import"
+
+FileUtils.mkdir_p [ STORAGE, TAGS, TRACKING, IMPORT ].map { |dir| "#{Dir.pwd}/#{dir}" }
 
 class Util
     class << self
@@ -96,6 +101,71 @@ class Tag
             end
 
             array.uniq
+        end
+    end
+end
+
+class Storage
+    class << self
+        def import directory = "#{Dir.pwd}/#{IMPORT}", root = directory
+            entries = Dir.entries(directory, { :encoding => "utf-8" })
+            total = entries.size - 2
+            index = 0
+
+            entries.each do |entry|
+                next if [ "..", "." ].include? entry
+
+                index += 1
+                entry = "#{directory}/#{entry}"
+
+                printf "(%#{total.to_s.size}d/%d) ", index, total
+
+                if File.directory? entry
+                    Storage.import entry, root
+
+                    if (Dir.entries(entry) - [ "..", "." ]).size.eql? 0
+                        Dir.delete entry
+                    end
+                end
+
+                if File.file? entry
+                    hash = Digest::SHA1.new
+                    file = File.open entry, "rb"
+                    target = Util.relative_path entry, root
+
+                    until file.eof?
+                        buffer = file.readpartial 65536
+
+                        hash.update buffer
+                    end
+
+                    file.close
+
+                    name = hash.hexdigest + Digest::SHA1.new.update(target).hexdigest
+                    link = "#{root}/../#{TRACKING}/#{name}"
+                    duplicate = false
+
+                    unless File.exists? link
+                        file = File.open link, "wb"
+
+                        file.puts target
+                        file.close
+                    else
+                        if File.exists? "#{root}/../#{STORAGE}/#{name}"
+                            File.delete entry
+
+                            duplicate = true
+
+                            next
+                        end
+                    end
+
+                    File.rename entry, "#{root}/../#{STORAGE}/#{name}"
+                    File.open("#{root}/../#{TAGS}/untagged/#{name}", "w").close
+
+                    puts "stored#{duplicate ? " duplicate " : " "}#{name} (#{target})"
+                end
+            end
         end
     end
 end
